0c4de74ec4612e95063a7199a333f5cab2808c18
Dhuvarakesan Mani <dhuvarakesan.mani@expleogroup.com>
2026-02-12 22:25:32 +0530

    axios instance updated

--- file-processing-service/app/utils/axios.instance.js
+++ file-processing-service/app/utils/axios.instance.js
@@ -1,81 +1,63 @@
-// import axios from "axios";
-// import https from "https";
-// import config from "../../config/config.js";
-// import logger from "./logger.js";
-
-// let httpsAgent;
-
-// if (config.externalApi.rejectUnauthorized === false) {
-//   logger.warn("⚠ SSL verification disabled for External API");
-
-//   httpsAgent = new https.Agent({
-//     rejectUnauthorized: false,
-//   });
-// }
-
-// const instance = axios.create({
-//   timeout: config.externalApi.timeout,
-//   httpsAgent,
-// });
-
-// /* ---------------------------
-//    REQUEST INTERCEPTOR
-// ---------------------------- */
-// instance.interceptors.request.use(
-//   (req) => {
-//     logger.debug("External API Request", {
-//       method: req.method,
-//       url: req.baseURL + req.url,
-//     });
-//     return req;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
-// /* ---------------------------
-//    RESPONSE INTERCEPTOR
-// ---------------------------- */
-// instance.interceptors.response.use(
-//   (res) => {
-//     logger.debug("External API Response", {
-//       status: res.status,
-//       url: res.config.url,
-//     });
-//     return res;
-//   },
-//   (error) => {
-//     if (error.code === "ECONNABORTED") {
-//       logger.error("External API Timeout", error.message);
-//     } else {
-//       logger.error("External API Error", {
-//         message: error.message,
-//         url: error.config?.url,
-//         status: error.response?.status,
-//       });
-//     }
-
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default instance;
-
 import axios from "axios";
 import https from "https";
+import config from "../../config/config.js";
+import logger from "./logger.js";
 
-let axiosInstance;
+let httpsAgent;
 
-try {
-    console.info('Creating Axios instance with custom HTTPS agent');
-    const httpsAgent = new https.Agent({
-        rejectUnauthorized: false, // (NOTE: this will disable client verification)
-    });
+if (config.externalApi.rejectUnauthorized === false) {
+  logger.warn("⚠ SSL verification disabled for External API");
 
-    axiosInstance = axios.create({ httpsAgent });
-    console.info('Axios instance created successfully');
-} catch (error) {
-    console.error('Error creating Axios instance:', (error).message);
-    throw error;
+  httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+  });
 }
 
+const axiosInstance = axios.create({
+  // timeout: config.externalApi.timeout,
+  httpsAgent,
+});
+
+/* ---------------------------
+   REQUEST INTERCEPTOR
+---------------------------- */
+axiosInstance.interceptors.request.use(
+  (req) => {
+    logger.debug("External API Request", {
+      method: req.method,
+      url: req.baseURL + req.url,
+    });
+    return req;
+  },
+  (error) => Promise.reject(error)
+);
+
+/* ---------------------------
+   RESPONSE INTERCEPTOR
+---------------------------- */
+axiosInstance.interceptors.response.use(
+  (res) => {
+    logger.debug("External API Response", {
+      status: res.status,
+      url: res.config.url,
+    });
+    return res;
+  },
+  (error) => {
+    if (error.code === "ECONNABORTED") {
+      logger.error("External API Timeout", error.message);
+    } else {
+      logger.error("External API Error", {
+        message: error.message,
+        url: error.config?.url,
+        status: error.response?.status,
+      });
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export { axiosInstance };
+
+
